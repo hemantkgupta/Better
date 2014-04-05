1831d7023ede30ad2ad7d61c4db814c88720717a
HemantKGupta <rajni.hemant@gmail.com>
2014-04-05 22:24:04 +0530

    added pattern search examples

--- 01-Arrays/src/CountSort.java
+++ 01-Arrays/src/CountSort.java
@@ -1,56 +1,54 @@
+import java.util.Arrays;
 
 public class CountSort {
 
 	public static void main(String[] args) {
-		char[] arr  = "geeksforgeeks".toCharArray();
+		int[] arr = { 2, 2, 2, 1, 4 };
+		System.out.println("\nAfter sorting elemnets are: ");
 		printArray(countSort(arr));
 	}
-	
-	// The main function that sort the given string str in alphabatical order
-	public static char[] countSort(char[] str)
-	{
-	    // The output character array that will have sorted str
-	    char[] output = new char[str.length];
-	 
-	    // Create a count array to store count of inidividul characters and
-	    // initialize count array as 0
-	    int[] count= new int[256];
-	    int i;
-	   
-	 
-	    // Store count of each character
-	    for(i = 0; i< str.length; ++i){
-	    	System.out.println("Char at: "+ i+" is: "+str[i]);
-	    	++count[str[i]];
-	    }
-	        
-	 
-	    // Change count[i] so that count[i] now contains actual position of
-	    // this character in output array
-	    for (i = 1; i <= 255; ++i)
-	        count[i] += count[i-1];
-	 
-	    // Build the output character array
-	    for (i = 0; i< str.length; ++i)
-	    {
-	        output[count[str[i]]-1] = str[i];
-	        --count[str[i]];
-	    }
-	 
-	    // Copy the output array to str, so that str now
-	    // contains sorted characters
-	    for (i = 0; i< str.length; ++i)
-	        str[i] = output[i];
-	    
-	    return output;
-	}
-	
-	public static void printArray(char[] arr){
-		System.out.println("");
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(" "+arr[i]);
-		}
+
+	public static int[] countSort(int[] arrs) {
 		
+		int n = arrs.length;
+		int[] output = new int[n];
+		int[] count = new int[n];
+		
+		int i;
+		for (i = 0; i < n; i++) {
+			++count[arrs[i]];
+		}
+
+		for (i = 1; i < n; i++)
+			count[i] += count[i - 1];
+
+		/*int j = 0;
+		for (i = 0; i < n; i++)
+			while (j < count[i])
+				output[j++] = i;*/
+		
+		for (i = 0; i< n; ++i)
+	    {
+			int element = arrs[i];
+	        int locationInOutput = count[element]-1;
+	       
+	        
+			output[locationInOutput] = element;
+			
+	        --count[element];
+	    }
+
+		for (i = 0; i < n; ++i)
+			arrs[i] = output[i];
+
+		return output;
+	}
+
+	public static void printArray(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
 	}
 
 }
