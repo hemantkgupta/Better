691682c8501c592dad9eafe05336aeea2ab4e076
HemantKGupta <rajni.hemant@gmail.com>
2014-04-14 23:58:28 +0530

    added tree prgrams

---
+++ CodilityTest/src/Alpha2010.java
@@ -0,0 +1,9 @@
+
+public class Alpha2010 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ CodilityTest/src/TreeHeight.java
@@ -0,0 +1,8 @@
+
+public class TreeHeight {
+
+	public static void main(String[] args) {
+
+	}
+
+}
