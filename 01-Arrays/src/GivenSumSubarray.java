5384ac9081d16e5b0f11ec7a020431b0ff465428
HemantKGupta <rajni.hemant@gmail.com>
2014-04-25 02:01:50 +0530

    added more arrays programs

--- 01-Arrays/src/GivenSumSubarray.java
+++ 01-Arrays/src/GivenSumSubarray.java
@@ -5,38 +5,24 @@ public class GivenSumSubarray {
 		subArraySum(arr, arr.length, 23);
 	}
 
-	/*
-	 * Returns true if the there is a subarray of arr[] with sum equal to 'sum'
-	 * otherwise returns false. Also, prints the result
-	 */
 	public static boolean subArraySum(int arr[], int n, int sum) {
-		
-		int curr_sum = arr[0], start = 0, i;
 
-		/*
-		 * Add elements one by one to curr_sum and if the curr_sum exceeds the
-		 * sum, then remove starting element
-		 */
+		int currsum = arr[0], start = 0, i;
+
+		// if currsum exceeds then remove starting element
+
 		for (i = 1; i <= n; i++) {
-			// If curr_sum exceeds the sum, then remove the starting elements
-			while (curr_sum > sum && start < i - 1) {
-				curr_sum = curr_sum - arr[start];
+			while (currsum > sum && start < i - 1) {
+				currsum = currsum - arr[start];
 				start++;
 			}
-
-			// If curr_sum becomes equal to sum, then return true
-			if (curr_sum == sum) {
-				System.out.printf("Sum found between indexes %d and %d", start,
-						i - 1);
+			if (currsum == sum) {
+				System.out.printf("Sum indexes %d and %d", start, i - 1);
 				return true;
 			}
-
-			// Add this element to curr_sum
 			if (i < n)
-				curr_sum = curr_sum + arr[i];
+				currsum = currsum + arr[i];
 		}
-
-		// If we reach here, then no subarray
 		System.out.printf("No subarray found");
 		return false;
 	}
--- 01-Arrays/src/KthSmallest.java
+++ 01-Arrays/src/KthSmallest.java
@@ -1,57 +1,49 @@
-import java.util.Arrays;
-
 
 public class KthSmallest {
 
 	public static void main(String[] args) {
-		
-		klargest(4);
+		int[] arr = { 20, 50, 60, 40, 90, 30, 70, 10 };
+		System.out.println(kthSmallest(arr, 8));
 
 	}
-	public static void klargest(int k){
-		int[] arr = {20, 50, 10, 40, 90, 30, 70, 60 };
-		buildHeap(arr, k);
+
+	public static int kthSmallest(int[] arr, int k) {
+		buildMaxHeap(arr, k);
 		for (int i = k; i < arr.length; i++) {
-			if(arr[i] > arr[0]){
-				arr[0]= arr[i];
-				minHeapify(arr,0,k);
+			if (arr[i] < arr[0]) {
+				arr[0] = arr[i];
+				maxHeapify(arr, 0, k);
 			}
 		}
-		System.out.println(Arrays.toString(arr));
+		return arr[0];
 	}
 	
-	public static void minHeapify(int[] arr, int i, int heapSize)
-	{
-	    int l = i*2 + 1;
-	    int r = i*2 + 2;
-	    int smallest = i;
-	    if (l < heapSize && arr[l] < arr[i])
-	        smallest = l;
-	    if (r < heapSize && arr[r] < arr[smallest])
-	        smallest = r;
-	    if (smallest != i)
-	    {
-	        swap(arr, i, smallest);
-	        minHeapify(arr, smallest, heapSize);
-	    }
+	public static void buildMaxHeap(int[] arr, int n) {
+		int i = n / 2;
+		while (i >= 0) {
+			maxHeapify(arr, i, n);
+			i--;
+		}
 	}
-	 
-	// A utility function to convert harr[] to a max heap
-	public static void buildHeap(int[] arr, int n)
-	{
-	    int i = (n - 1)/2;
-	    while (i >= 0)
-	    {
-	        minHeapify(arr, i, n);
-	        i--;
-	    }
+
+	public static void maxHeapify(int[] arr, int i, int heapSize) {
+		int l = i * 2 + 1;
+		int r = i * 2 + 2;
+		int largest = i;
+		if (l < heapSize && arr[l] > arr[i])
+			largest = l;
+		if (r < heapSize && arr[r] > arr[largest])
+			largest = r;
+		if (largest != i) {
+			swap(arr, i, largest);
+			maxHeapify(arr, largest, heapSize);
+		}
 	}
-	
-	public static void swap(int[] arr, int i, int j){
+
+	public static void swap(int[] arr, int i, int j) {
 		int temp = arr[i];
 		arr[i] = arr[j];
-		arr[j] = temp; 
+		arr[j] = temp;
 	}
-	
 
 }
